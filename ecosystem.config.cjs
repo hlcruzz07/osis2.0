36f81aa3ccf5837963b483e3e6c4003f52b68257
harold <harold.cruz0407@gmail.com>
2026-09-04 16:35:29 +0800

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,12 @@ module.exports = {
     apps: [
         {
             name: 'OSIS 2.0',
-            script: 'artisan',
-            interpreter: 'php',
-            args: 'queue:work --tries=3 --timeout=90',
+            script: '/opt/cpanel/ea-php83/root/usr/bin/php',
+            interpreter: 'none',
+            args: 'artisan queue:work --tries=3 --timeout=90',
+            cwd: '/home/syndicateadmin/public_html/subdomains/osiss2.0',
             exec_mode: 'fork',
-            instances: 1, // Increase this to run multiple workers
+            instances: 1,
             autorestart: true,
             watch: false,
             max_memory_restart: '200M',
